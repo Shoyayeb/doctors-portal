bb2808abee94b22e974b85c57324dfb5c6fd967c
Shoyayeb <sa.fine@outlook.com>
2021-11-27 03:23:04 +0600

    initial commit

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation />
+            <p>home</p>
+        </div>
+    );
+};
+
+export default Home;
