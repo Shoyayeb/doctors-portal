0b923ed32e1798b6eaa135728edce8c035720626
Shoyayeb <sa.fine@outlook.com>
2021-11-27 06:10:23 +0600

    services done

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,6 +1,7 @@
 import { Container } from '@mui/material';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 import React from 'react';
 import cavity from '../../../images/cavity.png';
 import fluoride from '../../../images/fluoride.png';
@@ -28,11 +29,17 @@ const Services = () => {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <Container>
+                <Typography variant="h6" component="div" sx={{ fontWeight: 500, m: 2, color: 'success.main' }} >
+                    OUR SERVICES
+                </Typography>
+                <Typography variant="h4" sx={{ fontWeight: 600, m: 5 }} component="div">
+                    Services we provide
+                </Typography>
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
                     {services.map(service => <Service key={service.name} service={service} ></Service>)}
                 </Grid>
             </Container>
-        </Box>
+        </Box >
     );
 };
 
