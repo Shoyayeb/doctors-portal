8318d2999b3c8973ae03190ec6f8ace13792fbb4
Shoyayeb <sa.fine@outlook.com>
2021-11-27 06:01:29 +0600

    services added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navigation from '../../Shared/Navigation/Navigation';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
-            <Navigation />
+            <Navigation></Navigation>
             <p>home</p>
+            <Services></Services>
         </div>
     );
 };
